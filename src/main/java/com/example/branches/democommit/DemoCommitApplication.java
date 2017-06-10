87e97ab371fd2ce6ef620bfde7b73a6599cffe33
Jorge Manrique <jormangon@gmail.com>
2017-06-10 11:42:51 +0100

    first commit

---
+++ src/main/java/com/example/branches/democommit/DemoCommitApplication.java
@@ -0,0 +1,12 @@
+package com.example.branches.democommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCommitApplication.class, args);
+	}
+}
